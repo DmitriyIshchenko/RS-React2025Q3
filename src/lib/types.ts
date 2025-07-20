0a793aed6c4b9a622daf8e1e4f7aa19ec4b2f921
Dmitry Ishchenko <19191458+DmitriyIshchenko@users.noreply.github.com>
2025-07-20 19:40:18 +0300

    Revert "refactor: remove comments"

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,29 @@
+// export interface Character {
+//   name: string;
+//   height: string;
+//   mass: string;
+//   hair_color: string;
+//   skin_color: string;
+//   eye_color: string;
+//   birth_year: string;
+//   gender: string;
+//   homeworld: string;
+//   films: string[];
+//   species: [];
+//   vehicles: [];
+//   starships: string[];
+//   created: string;
+//   edited: string;
+//   url: string;
+// }
+
+// export interface ApiResponse {
+//   count: number;
+//   next: string | null;
+//   previous: string | null;
+//   results: Character[];
+// }
+
 export interface ResourceBase {
   id: number;
   name: string;
